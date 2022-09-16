ea3d8a2432da3cbef30635915bf40f7c8d067358
Lucas Chao <lucaschao@outlook.com>
2022-09-16 16:39:32 +1000

    singleton pattern

---
+++ Singleton/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Logger
+    {
+        private String fileName;
+        private String logText;
+
+        private static Dictionary<string, Logger>  loggers = new Dictionary<string, Logger>();   
+
+        private Logger(String fileName)
+        {
+            this.fileName = fileName;
+            logText = string.Empty;
+        }
+
+        public static Logger GetInstance(string fileName) 
+        {
+            if (loggers.ContainsKey(fileName)) 
+            {
+                return loggers[fileName];
+            }
+
+            var newLogger = new Logger(fileName);
+
+            loggers.Add(fileName, newLogger);
+
+            return newLogger;
+        }
+
+
+        public void write(String message)
+        {
+            logText += $" {message} {DateTime.Now.ToString()}";
+
+            Console.WriteLine("log: " + logText);
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Singleton;
+
+Logger logger = Logger.GetInstance("file1");
+
+logger.write("something...1");
+
+logger.write("something...2");
+
+Logger logger2 = Logger.GetInstance("file1");
+
+logger2.write("something...3");
+
+Logger logger3 = Logger.GetInstance("file2");
+
+logger3.write("something meaningful...");
+
